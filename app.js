e8f2161f8b28608fc4e204168703d1f0462c5740
Gulshan kumar <test.gulshan@gmail.com>
2016-09-02 00:12:31 +0530

    Setup Passport to Handle the Express Authentication

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var passport = require('passport');
+
+require('./app_api/models/db');
+require('./app_api/config/passport');
+
+app.use(passport.initialize());
+app.use('/api', routesApi);
